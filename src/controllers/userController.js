373cfcbc638f4b690d9e9be683ce0ab8101a70f3
ziad-zezo <20224264@fue.edu.eg>
2026-08-18 20:21:34 +0300

    feat: add user profile retrieval functionality in userController and userRoutes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,6 +4,24 @@ import User from "../models/User.js";
 import jwt from 'jsonwebtoken';
 
 
+
+export const profile = async (req, res) => {
+    try {
+        const userId = req.user.id;
+        const user = await User.findById(userId).select('-password');
+        if (!user) {
+            return res.status(404).json({ success: false, message: "User not found" });
+        }
+        res.status(200).json({ success: true, user });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+}
+
+
 export const getUsers = async (req, res) => {
     try {
         const users = await User.find();
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getUsers, getUser, updateUser, deleteUser } from '../controllers/userController.js';
+import { getUsers, getUser, updateUser, deleteUser ,profile} from '../controllers/userController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 router.get('/', protect, getUsers);
+router.get('/profile', protect, profile);
 router.get('/:id', protect, getUser);
 router.patch('/:id', protect, updateUser);
 router.delete('/:id', protect, deleteUser);
